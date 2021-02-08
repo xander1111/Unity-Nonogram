feca02487df449ac3c48379ec5ddb8684f17e00e
Xander Bailey <xander.bailey@gmail.com>
2021-02-07 16:31:16 -0800

    Improved board generation.
    I realized it would be a lot easier to make all tiles 1 unit by 1 unit and zoom the camera to fit it, rather than scaling the board to fit the camera.
    I also changed how rows and columns of tiles are found.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,27 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Text.RegularExpressions;
 
 public class GameManager : MonoBehaviour
 {
+    public bool helpMode = true;
+    public TMPro.TextMeshProUGUI mistakeCounter;
+
     private Tile[] _tiles;
+    
 
     private void Start()
     {
         _tiles = FindObjectsOfType<Tile>();
     }
 
-
-    // Checks each tile to see if its solved or not
+    
     public bool CheckIfSolved()
     {
-        bool solved = false;
-
+        // Checks each tile to see if its solved or not
         foreach (Tile tile in _tiles)
         {
-            solved = tile.solved == tile.state;
+            if (tile.solved != tile.State)
+            {
+                return false;
+            }
         }
 
-        return solved;
+        return true;
+    }
+
+
+    public void AddMistake()
+    {
+        // In helpMode, the game lets the player know they got a tile wrong and adds 1 to a mistake counter
+        int oldNum = System.Convert.ToInt32(Regex.Match(mistakeCounter.text, @"\d+").Value);
+        string newNum = (oldNum + 1).ToString();
+        mistakeCounter.text = Regex.Replace(mistakeCounter.text, @"\d+", newNum);
     }
 }
--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -11,13 +11,35 @@ public class Tile : MonoBehaviour
     // Indicates the solved state for this tile
     public int solved;
     // Indicates the tiles current state
-    public int state = 0;
+    protected int _state = 0;
     /*
      *  state ids:
      *   0: empty
      *  -1: x
      *  1: filled
      */
+    public int State {
+        get { return _state; }
+        set 
+        { 
+            _state = value;
+            switch (value)
+            {
+                case 0:
+                    _sr.sprite = empty;
+                    break;
+                case 1:
+                    _sr.sprite = filled;
+                    break;
+                case -1:
+                    _sr.sprite = x;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+    
 
     private SpriteRenderer _sr;
     private GameManager _gm;
@@ -32,40 +54,61 @@ public class Tile : MonoBehaviour
 
     private void OnMouseOver()
     {
-        //Checks for left click
-        if (Input.GetMouseButtonDown(0))
+        // In help mode, when the tile is solved, don't let the player change it
+        if (State == 0 || !_gm.helpMode)
         {
-            //Resets the tile if state isnt empty already
-            if (state != 0)
+            //Checks for left click
+            if (Input.GetMouseButtonDown(0))
             {
-                state = 0;
-                _sr.sprite = empty;
-            }
-            else
-            //Sets the tile state to filled
-            {
-                state = 1;
-                _sr.sprite = filled;
-            }
+                //Resets the tile if state isnt empty already
+                if (State != 0)
+                {
+                    State = 0;
+                }
+                else
+                //Sets the tile state to filled
+                {
+                    State = 1;
+                }
 
-            //Checks whether the puzzle has been solved
-            _gm.CheckIfSolved();
-        } 
-        //Repeats process for right click
-        else if (Input.GetMouseButtonDown(1))
+                //Checks whether the tile has been solved
+                CheckIfTileSolved(this);
+            }
+            //Repeats process for right click
+            else if (Input.GetMouseButtonDown(1))
+            {
+                if (State != 0)
+                {
+                    State = 0;
+                }
+                else
+                {
+                    State = -1;
+                }
+
+                CheckIfTileSolved(this);
+            }
+        }
+    }
+
+
+    private bool CheckIfTileSolved(Tile tile)
+    {
+        if (tile.solved == tile.State)
         {
-            if (state != 0)
-            {
-                state = 0;
-                _sr.sprite = empty;
-            }
-            else
-            {
-                state = -1;
-                _sr.sprite = x;
-            }
-
+            // If this tile got solved, check if the whole puzzle is solved as a result
             _gm.CheckIfSolved();
+            return true;
+        }
+        else
+        {
+            if (_gm.helpMode)
+            {
+                _gm.AddMistake();
+                State = 0;
+            }
+            
+            return false;
         }
     }
 }
